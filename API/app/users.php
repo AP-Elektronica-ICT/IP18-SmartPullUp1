eea20a286b394b95c66b809ccd1c023e83c19a7e
Unknown <nemesruben@gmail.com>
2018-03-04 18:42:53 +0100

    Installation instructions in README

--- API/app/users.php
+++ API/app/users.php
@@ -1,4 +1,38 @@
 <?php
 $app->get('/users', function(){
-    echo "welcome to users";
+    // connect
+    $m = new MongoClient();
+    // select your database
+        $db = $m->smartpullupbar;
+    // select your collection
+        $collection = $db->users;
+    // add a record
+        $json = '{
+      "userid": "hjkdh32jhjfshjdsfk",
+      "name": "Ruben Nemes",
+      "age": 22,
+      "weight": 75,
+      "pullups": {
+        "1520182860": {
+          "pullup1": [
+            4.9,
+            6.1
+          ],
+          "pullup2": [
+            8.4,
+            9.3
+          ],
+          "pullup3": [
+            11.7,
+            12.9
+          ],
+          "pullup4": [
+            18.4,
+            19
+          ]
+        }
+      }
+    }';
+        $document = json_decode($json);
+        $collection->insert($document);
 });
