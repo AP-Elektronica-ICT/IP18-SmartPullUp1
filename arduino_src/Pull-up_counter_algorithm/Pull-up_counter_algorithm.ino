a4fa0bd4c4b7428716562767fd96e884e38dd607
Unknown <tuomas.isoniemi@outlook.com>
2018-03-27 12:01:42 +0300

    fine tuning to code

--- arduino_src/Pull-up_counter_algorithm/Pull-up_counter_algorithm.ino
+++ arduino_src/Pull-up_counter_algorithm/Pull-up_counter_algorithm.ino
@@ -4,50 +4,46 @@
 MedianFilter median(5, 460);
 
 double sampling = 5; 
+double sampleTime = 0;
+double Start = 0;
+
+float average = 0.0;
+float feedback = 0.0;
+
 int oldInput = 0;
 int input=0;
-double sampleTime = 0;
 int filteredOutput=0;
 int output;
 int i = 0;
-float average = 0.0;
-float feedback = 0.0;
-int old_func_value = 0;
-boolean Flag = true;
 int Count = 0;
-double Start = 0;
+int old_func_value = 0;
+int ref_rest_avg = 0;
+
+boolean Flag = true;
 int oldout = 0;
-int newValue[10];
-int oldValue[7];
 
-void setup() {
-Serial.begin(38400);
-MsTimer2::set(sampling,measure);
-MsTimer2::start();
-pinMode(A0,INPUT);
+void filter()
+{
+for(i = 0; i < 30; i++)
+  {
+    input = analogRead(A0);
+    median.in(input);
+    input = median.out();
+    average = average + input;
+  }
+average = average / 30.0;
+feedback = feedback * 0.8 + 0.2 * average;  
 }
-
 
 void measure(){
 
-  for(i = 0; i < 30; i++)
-    {
-      input = analogRead(A0);
-      median.in(input);
-      input = median.out();
-      average = average + input;
-    }
-  average = average / 30.0;
-  feedback = feedback * 0.8 + 0.2 * average;
-  sampleTime += sampling / 1000;
-  output = feedback;
-  
-  
+filter();
 
-
-  if(oldout > output * 0.7 && sampleTime > 30)
-    {
-      if(Start==0)
+sampleTime += sampling / 1000;
+  
+if(oldout > output * 0.7 && sampleTime > 30)
+  {
+    if(Start==0)
       {
         Start = sampleTime;
       }  
@@ -55,13 +51,13 @@ void measure(){
       {
         Counter(output);
       }
-    }
+  }
    
 Serial.print(Start);
 Serial.print("\t");
 Serial.print(output);
-Serial.print("\t");
-Serial.print(oldout);
+//Serial.print("\t");
+//Serial.print(oldout);
 Serial.print("\t");
 Serial.println(sampleTime);
 //Serial.print("\t");
@@ -106,21 +102,22 @@ int Counter(int value){
   return;
 }
 
-void ref_measure(){
-  for(int n = 0; n < 10; n++)
-  {
-    for(i = 0; i < 30; i++)
-      {
-        input = analogRead(A0);
-        median.in(input);
-        input = median.out();
-        average = average + input;
-      }
-    average = average / 30.0;
-    feedback = feedback * 0.8 + 0.2 * average;
-//    ref_array[n] = feedback;
-//    muuttuja += ref_array[n];
-  }
+
+void setup() {
+Serial.begin(38400);
+
+for(int n=0; n<100; n++)
+{
+  filter();
+  ref_rest_avg += feedback;
+  if(n==99) 
+    ref_rest_avg/100;
 }
+Serial.println(ref_rest_avg);
+MsTimer2::set(sampling,measure);
+MsTimer2::start();
+pinMode(A0,INPUT);
+}
+
 void loop() {
 }
