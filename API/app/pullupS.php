12cc6a9d2365bb3cce255e7362168b35e33270f6
Unknown <nemesruben@gmail.com>
2018-04-17 21:27:21 +0200

    post data added to save pullupsession

--- API/app/pullupS.php
+++ API/app/pullupS.php
@@ -1,20 +1,6 @@
 <?php
 $app->get('/pullups', function($request, $response, $args){
     $collection = (new MongoDB\Client)->smartpullupbar->users;
-    $id = "google-oauth2|116967247859714699456";
     
-    $pullupS = array('$set' => array("timestamp" => [
-        "amount" => 6,
-        "duration" => 5,
-        "avgspeed" => 5,
-        "weight" => 180,
-        "completion" => 70,
-        "goal" => 20
-    ]));
-    var_dump($pullupS);
-    try {
-    $collection->updateOne(array("userid" => $id), $pullupS);
-    } catch (Exeception $e) {
-        var_dump($e);
-    }
+    var_dump($POST);
 });
