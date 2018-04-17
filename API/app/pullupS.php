e2064e21c25f4c3cdedee031f5eb85a7a512ffa5
Unknown <nemesruben@gmail.com>
2018-04-17 21:37:20 +0200

    modified api to accept post requests

--- API/app/pullupS.php
+++ API/app/pullupS.php
@@ -1,6 +1,18 @@
 <?php
-$app->get('/pullups', function($request, $response, $args){
+$app->post('/pullups', function($request, $response, $args){
     $collection = (new MongoDB\Client)->smartpullupbar->users;
-    
-    var_dump($POST);
+    $pullupS = array('$set' => array($_POST['timestamp'] => [
+        "amount" => $_POST['amount'],
+        "duration" => $_POST['duration'],
+        "avgspeed" => $_POST['avgspeed'],
+        "weight" => $_POST['weight'],
+        "completion" => $_POST['completion'],
+        "goal" => $_POST['goal']
+    ]));
+    var_dump($pullupS);
+    try {
+        $collection->updateOne(array("userid" => $id), $pullupS);
+    } catch (Exeception $e) {
+        var_dump($e);
+    }
 });
--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -15,8 +15,17 @@ export class ApiService {
         });
     }
 
-    public insertPullupSession(id: string, timestamp: number, duration: number, avgspeed: number, weight: number, completion: number, goal: number ){
-        let data = 'userid='+id+'&timestamp='+String(timestamp)+'&duration='+String(duration)+'&avgspeed='+String(avgspeed)+'&weight='+String(weight)+'&completion='+String(completion)+'&goal='+String(goal);
+    public insertPullupSession(id: string, amount : number, timestamp: number, duration: number, avgspeed: number, weight: number, completion: number, goal: number ){
+        let data = {
+            'userid' : id,
+            'timestamp' : String(timestamp),
+            'amount' : String(amount),
+            'duration' : String(duration),
+            'avgspeed' : String(avgspeed),
+            'weight' : String(weight),
+            'completion' : String(completion),
+            'goal' : String(goal)
+        }
         this._http.post('http://ec2-54-77-199-101.eu-west-1.compute.amazonaws.com/pullups/', data);
     }
 }
