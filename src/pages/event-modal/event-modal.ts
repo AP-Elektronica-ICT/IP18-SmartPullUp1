936534819136b2a2990e61aefa4fbb263ccf855c
Unknown <nemesruben@gmail.com>
2018-04-17 13:42:09 +0200

    alert box to view event

--- src/pages/event-modal/event-modal.ts
+++ src/pages/event-modal/event-modal.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import * as moment from 'moment';
 
-/**
- * Generated class for the EventModalPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +10,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EventModalPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  event = { startTime: new Date().toISOString(), endTime: new Date().toISOString(), allDay: false };
+  minDate = new Date().toISOString();
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+    let preselectedDate = moment(this.navParams.get('selectedDay')).format();
+    this.event.startTime = preselectedDate;
+    this.event.endTime = preselectedDate;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventModalPage');
+  }
+
+  cancel() {
+    this.viewCtrl.dismiss();
+  }
+ 
+  save() {
+    this.viewCtrl.dismiss(this.event);
   }
 
 }
